12eb54772edfa9f7bd96e97e091f437224f4d629
florentinobenedictus <85059763+florentinobenedictus@users.noreply.github.com>
2022-04-10 19:04:43 +0700

    Create soal5.r

---
+++ soal5.r
@@ -0,0 +1,25 @@
+#No. 5
+#5A
+soal_5a<-function(x,lambda){
+  #Nilai x dapat diganti karena belum ada di soal
+  dexp(x,rate=lambda)
+}
+soal_5a(5,3)
+
+#5B
+soal_5b<-function(x,lambda){
+  set.seed(1)
+  hist(rexp(x,rate=lambda))
+}
+soal_5b(10,3)
+soal_5b(100,3)
+soal_5b(1000,3)
+soal_5b(10000,3)
+
+#5C
+soal_5c<-function(lambda){
+  rataan=1/lambda
+  varian=1/(lambda^2)
+  cat("3D. Rataan:",rataan,", dan varian:",varian,"\n")
+}
+soal_5c(3)
